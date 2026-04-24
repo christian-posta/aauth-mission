ce365245c796103a7fdb2ca3243823f5c018254d
Christian Posta <christian.posta@gmail.com>
2026-04-23 18:07:28 -0700

    added helper script for running server in default mode

---
+++ run-server.sh
@@ -0,0 +1,7 @@
+AAUTH_PS_PUBLIC_ORIGIN=http://127.0.0.1:8765 \
+AAUTH_AS_PUBLIC_ORIGIN=http://127.0.0.1:8765 \
+AAUTH_PS_ADMIN_TOKEN=mytoken \
+AAUTH_AS_PERSON_TOKEN=mytoken \
+AAUTH_PS_INSECURE_DEV=false \
+AAUTH_AS_INSECURE_DEV=false \
+uvicorn portal.http.app:app --host 127.0.0.1 --port 8765
